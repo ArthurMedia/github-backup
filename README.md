a770f17637f6a84bc4f812288efe1052b94122b3
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2014-01-03 15:56:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Github backup
+
+Makes a local backup copy of all of a Github user's (or Github organization's)
+repositories.
+
+## Install
+
+```
+pip install -r requirements.txt
+export GITHUB_BACKUP_DIR=/backups/github
+export GITHUB_TOKEN=xxx
+python github_backup.py
+```
---
+++ github_backup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import subprocess
+import requests
+import syslog
+import sys
+import os
+
+backup_dir = os.environ.get('GITHUB_BACKUP_DIR', '/tmp/github_backup')
+token = os.environ.get('GITHUB_TOKEN')
+urls = [
+    'https://api.github.com/orgs/ArthurMediaGroup/repos',
+    'https://api.github.com/user/repos'
+]
+
+
+def main():
+    if not os.path.isdir(backup_dir):
+        sys.exit("%s directory doesn't exist" % backup_dir)
+
+    for url in urls:
+        _backup(r=_repos(url))
+
+
+def _repos(url):
+    return requests.get(
+        url,
+        headers={'Authorization': 'token %s' % token}
+    )
+
+
+def _backup(r):
+    for repository in r.json():
+        url = repository['html_url'].replace(
+            'github.com',
+            '%s:x-oauth-basic@github.com' % token
+        )
+        split_url = url.split('/')
+        dirname = split_url[-1]
+        os.chdir(backup_dir)
+
+        if not os.path.isdir('%s/%s' % (backup_dir, dirname)):
+            try:
+                subprocess.call("git clone --mirror %s %s" % (url, dirname), shell=True)
+                syslog.syslog(syslog.LOG_INFO, "Git clone OK : %s" % dirname)
+            except:
+                syslog.syslog(syslog.LOG_CRIT, "Git clone FAILED : %s" % dirname)
+                pass
+
+        try:
+            os.chdir("%s/%s" % (backup_dir, dirname))
+            subprocess.call('git remote update &>/dev/null', shell=True)
+            syslog.syslog(syslog.LOG_INFO, "Git remote update OK : %s" % dirname)
+        except:
+            syslog.syslog(syslog.LOG_CRIT, "Git remote update FAILED : %s" % dirname)
+
+
+if __name__ == "__main__":
+    main()
